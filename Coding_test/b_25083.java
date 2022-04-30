5fcb0acef8b420b3293220e832d3bf8af58d26ba
KimKiBoum <03rlarlqja@naver.com>
2022-05-01 00:01:08 +0900

    b_25083

---
+++ Coding_test/b_25083.java
@@ -0,0 +1,12 @@
+package Coding_test;
+public class b_25083 {
+    public static void main(String[] args) {
+        System.out.printf("         ,r'\"7\n");
+        System.out.printf("r`-_   ,'  ,/\n");
+        System.out.printf(" \\. \". L_r'\n");
+        System.out.printf("   `~\\/\n");
+        System.out.printf("      |\n");
+        System.out.printf("      |\n");
+
+    }
+}
