b8afdc85bb1f35683ea94dfc88ff0a50cb4f0a29
KimKiBoum <03rlarlqja@naver.com>
2022-06-29 23:42:24 +0900

    [ADD] b_2558

---
+++ Coding_test/baekjoon/b_2558.java
@@ -0,0 +1,14 @@
+package Coding_test.baekjoon;
+
+import java.util.Scanner;
+
+public class b_2558 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a+b);
+    }
+}
