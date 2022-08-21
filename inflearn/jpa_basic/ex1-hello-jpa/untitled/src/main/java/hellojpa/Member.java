a47eea37dcc38b4ba3fe326d08c4dd9ed6090de0
KimKiBoum <03rlarlqja@naver.com>
2022-08-21 12:54:14 +0900

    [UPDATE] JPA 업데이트

---
+++ inflearn/jpa_basic/ex1-hello-jpa/untitled/src/main/java/hellojpa/Member.java
@@ -0,0 +1,31 @@
+package hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity()
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    private Long id;
+    @Column(name = "name")
+    private String username;
+    private Integer age;
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob
+    private String description;
+
+    public Member() {
+
+    }
+}
