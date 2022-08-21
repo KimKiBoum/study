bfdab78ed1678929c95139367b63071d5718630a
KimKiBoum <03rlarlqja@naver.com>
2022-08-22 00:25:37 +0900

    [UPDATE] JPA 업데이트

---
+++ inflearn/jpa_basic/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+
+    @Column(name = "ITEM_ID")
+    private Long itemId;
+    private int orderPrice;
+    private int count;
+}
